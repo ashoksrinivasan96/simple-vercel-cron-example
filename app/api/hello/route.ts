aee40c02aceaa8e93cb9d175f5de7aca3cdfc409
ashoksrinivasan96 <ashoksrinivasan96@gmail.com>
2024-09-18 12:22:11 +0530

    fixing author issue

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
-    //test123
+    //test1234
   return console.log("Testing Cron");
 }
