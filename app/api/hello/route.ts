147b2749acea7cf06d4d90800d08b9df8ae3f91a
ashoksrinivasan96 <ashoksrinivasan@Ashoks-MacBook-Pro.local>
2024-09-18 12:19:55 +0530

    testing git username

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
-    //test
+    //test123
   return console.log("Testing Cron");
 }
