fb6dabac6022a5c260246a7c365dd60c01c37031
ashoksrinivasan96 <ashoksrinivasan96@gmail.com>
2024-09-18 12:44:09 +0530

    fixing eslint issue

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,6 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
-export function GET(request: Request) {
+export function GET() {
     //test12345
   return console.log("Testing Cron");
 }
